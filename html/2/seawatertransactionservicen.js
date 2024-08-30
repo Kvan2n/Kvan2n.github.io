aad9d4f0fe4ba2306f198881f71f37389504727f
Kvan2n <102384837+Kvan2n@users.noreply.github.com>
2024-08-30 10:22:55 +0200

    Added two new pages
    you can now purchase seawater with polish currency as well as get your personal data stolen. on top of this it is also possible to accept the cookies

---
+++ html/2/seawatertransactionservicen.js
@@ -0,0 +1,9 @@
+function buy() {
+    console.log(":)")
+    document.getElementById("buybutton").style.display = "block"
+
+    setTimeout(function(){document.getElementById("buybutton").style.display = "none"}, 2000)
+
+}
+
+//this doesnt do shit anymore lmao
